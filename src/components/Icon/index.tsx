555a8ad6f7f025f2daea652fa5f22ded1666f49e
JoseAntpr <joseantpalaciosramirez@gmail.com>
2018-09-04 11:44:48 +0200

    ADD: icon hove style

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -6,13 +6,17 @@ const StyledIcon = styled.span`
     display: inline-flex;
     justify-content: center;
     align-items: center;
-    background-color: rgba(26, 187, 156, 0.65);
+    background-color: rgba(26, 187, 156);
     color: white;
     height: 36px;
     width: 36px;
     margin: 10px;
     border-radius: 50%;
     text-align: center;
+
+    &:hover {
+        background-color: rgba(17, 160, 132);
+    }
 `
 const Icon = ( {href = linkUrl, icon = 'fab fa-linkedin-in'} ) => (
     <a href={href} target="_blank" style={{ textDecoration: 'none'}}>
