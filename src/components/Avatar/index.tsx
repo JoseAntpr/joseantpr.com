a36ba38b34f1b3e765affb1c77f1839488f753cf
JoseAntpr <joseantpalaciosramirez@gmail.com>
2018-09-04 10:18:50 +0200

    ADD: add border to avatar & shadow

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -3,8 +3,10 @@ import { styled } from '../../utils/theme/themeStyledComponents';
 
 const imgUrl = 'https://media.licdn.com/dms/image/C4E03AQGUbpMGgZo7ag/profile-displayphoto-shrink_200_200/0?e=1541030400&v=beta&t=SToow7_rboLQLKMYPFi6IrQzp5o1vpa7vDYDj03ov-I';
 const StyledAvatar = styled.img`
+    border: 3px solid rgba(26, 187, 156);
     border-radius: 50%;
-    padding: 15px;
+    margin: 15px;
+    box-shadow: 2px 2px 0px 2px rgb(0, 0, 0, 0.3);
 `
 
 const Avatar = ({ img = imgUrl}) => (
